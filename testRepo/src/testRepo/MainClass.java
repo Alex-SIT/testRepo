95bd39e5fb83aec5d64c3ecaf6ecc10841a0fbf1
livingiron2 <cveganton2@gmail.com>
2022-03-17 17:10:51 +0200

    putin patchnote

--- testRepo/src/testRepo/MainClass.java
+++ testRepo/src/testRepo/MainClass.java
@@ -3,7 +3,7 @@ package testRepo;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is a hello");
+		System.out.println("This is Russian territory");
 
 	}
 
